16b73719330c38e89a7b54164e1c9b9048ea0695
김범중 <rlaqjawnd625@naver.com>
2024-09-02 13:07:23 +0900

    [BE] Init: Backend 프로젝트 초기설정

---
+++ backend/src/test/java/com/example/carpay/CarPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
