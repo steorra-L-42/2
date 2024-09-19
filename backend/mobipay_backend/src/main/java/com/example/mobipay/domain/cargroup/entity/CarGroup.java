b2fee328f5deee1b4cc92740b0fcaad302ff6a14
김범중 <rlaqjawnd625@naver.com>
2024-09-19 23:19:48 +0900

    [BE] Feat: CarGroup 엔티티에 car_id와 mobi_user_id를 복합키로 사용하기 위해 @IdClass(CarGroupId.class) 추가.

--- backend/mobipay_backend/src/main/java/com/example/mobipay/domain/cargroup/entity/CarGroup.java
+++ backend/mobipay_backend/src/main/java/com/example/mobipay/domain/cargroup/entity/CarGroup.java
@@ -2,9 +2,11 @@ package com.example.mobipay.domain.cargroup.entity;
 
 import com.example.mobipay.domain.car.entity.Car;
 import com.example.mobipay.domain.mobiuser.entity.MobiUser;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -15,10 +17,18 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@IdClass(CarGroupId.class)
 @Table(name = "car_group")
 public class CarGroup {
 
     @Id
+    @Column(name = "car_id")
+    private Long carId;
+    
+    @Id
+    @Column(name = "mobi_user_id")
+    private Long mobiUserId;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "car_id")
     private Car car;
