3ee653640f67252fcd5ad1d4e3ad0631ddfbc55e
김세진 <rlatpwls30@naver.com>
2024-09-19 18:00:34 +0900

    [AND] Refactor: 커스텀 ActionStrip 적용

--- android/MobiPay/features/freedrive/src/auto/java/com/kimnlee/freedrive/presentation/screen/FreeDriveCarScreen.kt
+++ android/MobiPay/features/freedrive/src/auto/java/com/kimnlee/freedrive/presentation/screen/FreeDriveCarScreen.kt
@@ -2,11 +2,16 @@ package com.kimnlee.freedrive.presentation.screen
 
 import androidx.annotation.UiThread
 import androidx.car.app.Screen
+import androidx.car.app.model.Action
+import androidx.car.app.model.ActionStrip
 import androidx.car.app.model.CarColor
+import androidx.car.app.model.CarIcon
 import androidx.car.app.model.Template
 import androidx.car.app.navigation.model.NavigationTemplate
+import androidx.core.graphics.drawable.IconCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import com.kimnlee.freedrive.R
 import com.mapbox.androidauto.MapboxCarContext
 import com.mapbox.androidauto.action.MapboxMapActionStrip
 import com.mapbox.androidauto.internal.logAndroidAuto
@@ -17,6 +22,7 @@ import com.mapbox.androidauto.navigation.roadlabel.CarRoadLabelRenderer
 import com.mapbox.androidauto.navigation.speedlimit.CarSpeedLimitRenderer
 import com.mapbox.androidauto.preview.CarRouteLineRenderer
 import com.mapbox.androidauto.screenmanager.MapboxScreen
+import com.mapbox.androidauto.screenmanager.MapboxScreenManager
 import com.mapbox.navigation.base.ExperimentalPreviewMapboxNavigationAPI
 
 /**
@@ -69,6 +75,47 @@ internal class FreeDriveCarScreen @UiThread constructor(
             mapboxCarContext.options.actionStripProvider
                 .getActionStrip(this, MapboxScreen.FREE_DRIVE)
 
+        fun customBuildSettingsAction() = Action.Builder()
+            .setIcon(
+                CarIcon.Builder(
+                    IconCompat.createWithResource(
+                        carContext,
+                        R.drawable.ic_settings
+                    )
+                ).build()
+            )
+            .setOnClickListener {
+                MapboxScreenManager.push(MapboxScreen.SETTINGS)
+            }
+            .build()
+
+        fun customBuildSearchAction(): Action = Action.Builder()
+            .setIcon(
+                CarIcon.Builder(
+                    IconCompat.createWithResource(
+                        carContext,
+                        R.drawable.ic_search_black36dp
+                    )
+                ).build()
+            )
+            .setOnClickListener {
+                MapboxScreenManager.push(MapboxScreen.SEARCH)
+            }
+            .build()
+
+        fun customBuildFavoritesAction(): Action = Action.Builder()
+            .setTitle("가맹점 전체보기")
+            .setOnClickListener {
+                MapboxScreenManager.push(MapboxScreen.FAVORITES)
+            }
+            .build()
+
+        val customFreeDriveActionStrip = ActionStrip.Builder()
+            .addAction(customBuildSettingsAction())
+            .addAction(customBuildSearchAction())
+            .addAction(customBuildFavoritesAction())
+            .build()
+
 //        actionStrip1
 //            .actions
 //            .add(
@@ -85,7 +132,7 @@ internal class FreeDriveCarScreen @UiThread constructor(
         return NavigationTemplate.Builder()
             .setBackgroundColor(CarColor.PRIMARY)
             .setActionStrip(
-                actionStrip1
+                customFreeDriveActionStrip
             )
             .setMapActionStrip(mapActionStripBuilder.build())
             .build()
