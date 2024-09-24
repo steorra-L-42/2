069ff58d844932420c86a860dda5af0532e058ba
이철민 <lstork48@gmail.com>
2024-09-24 13:20:54 +0900

    [AND] Test: 테스트용 회원가입 코드 추가 - 이름과 전화번호 입력 칸 추가 - api 통신 수정

--- android/MobiPay/common/src/main/java/com/kimnlee/common/auth/AuthManager.kt
+++ android/MobiPay/common/src/main/java/com/kimnlee/common/auth/AuthManager.kt
@@ -12,8 +12,7 @@ import kotlinx.coroutines.flow.map
 //테스트를 위한 회원가입용 임포트문 삭제예정(api 통신에 필요할 경우 남길 수 있음)
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.Retrofit
-import retrofit2.http.Body
-import retrofit2.http.POST
+import retrofit2.http.*
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "auth")
 
@@ -32,13 +31,13 @@ class AuthManager(private val context: Context) {
         }
     }
 
-    suspend fun signUp(email: String): SignUpResponse {
-        return authService.signUp(SignUpRequest(email))
+    suspend fun signUp(email: String, name: String, phoneNumber: String): SignUpResponse {
+        return authService.signUp(SignUpRequest(email, name, phoneNumber))
     }
 
     private fun createAuthService(): AuthService {
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://localhost:8080") // baseUrl 수정해야함ㄴ
+            .baseUrl("http://localhost:8080") // baseUrl 수정해야함
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         return retrofit.create(AuthService::class.java)
@@ -50,5 +49,5 @@ interface AuthService {
     suspend fun signUp(@Body request: SignUpRequest): SignUpResponse
 }
 
-data class SignUpRequest(val email: String)
+data class SignUpRequest(val email: String, val name: String, val phoneNumber: String)
 data class SignUpResponse(val success: Boolean, val message: String)
