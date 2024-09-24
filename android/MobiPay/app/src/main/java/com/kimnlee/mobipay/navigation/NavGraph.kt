8a97eca1b8230a37f904a378e5cc1d242be2e97f
Harvey <ssafy.lsc@gmail.com>
2024-09-24 17:08:43 +0900

    [AND] feat: SharedPreference에 저장된 마지막 주차 위치 HomeScreen에 표시

--- android/MobiPay/app/src/main/java/com/kimnlee/mobipay/navigation/NavGraph.kt
+++ android/MobiPay/app/src/main/java/com/kimnlee/mobipay/navigation/NavGraph.kt
@@ -1,5 +1,6 @@
 package com.kimnlee.mobipay.navigation
 
+import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -7,7 +8,6 @@ import androidx.compose.runtime.getValue
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navigation
 import com.kimnlee.auth.navigation.authNavGraph
 import com.kimnlee.auth.presentation.viewmodel.LoginViewModel
 import com.kimnlee.cardmanagement.navigation.cardManagementNavGraph
@@ -22,7 +22,8 @@ import com.kimnlee.vehiclemanagement.navigation.vehicleManagementNavGraph
 @Composable
 fun AppNavGraph(
     navController: NavHostController,
-    authManager: AuthManager
+    authManager: AuthManager,
+    context: Context
 ) {
     val loginViewModel = LoginViewModel(authManager)
     val isLoggedIn by loginViewModel.isLoggedIn.collectAsState()
@@ -45,7 +46,8 @@ fun AppNavGraph(
             ScreenWithBottomNav(navController) {
                 HomeScreen(
                     viewModel = loginViewModel,
-                    navController = navController
+                    navController = navController,
+                    context = context
                 )
             }
         }
