6da980919264210d14f10c4ffa36c7c8d915b9c0
김범중 <rlaqjawnd625@naver.com>
2024-09-26 17:57:31 +0900

    [BE] Feat: MobiUser 연관관계 설정 기능 추가

--- backend/mobipay_backend/src/main/java/com/example/mobipay/domain/mobiuser/repository/MobiUserRepository.java
+++ backend/mobipay_backend/src/main/java/com/example/mobipay/domain/mobiuser/repository/MobiUserRepository.java
@@ -2,6 +2,10 @@ package com.example.mobipay.domain.mobiuser.repository;
 
 import com.example.mobipay.domain.mobiuser.entity.MobiUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MobiUserRepository extends JpaRepository<MobiUser, Long> {
+
+    Boolean existsByEmail(String email);
 }
