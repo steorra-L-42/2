e86b316d6a3594ea5feac3a5d25a1d88c5e2c3fb
Harvey <ssafy.lsc@gmail.com>
2024-09-28 17:41:36 +0900

    [AND] refactor: 앱 아이콘, 이름 변경 및 버전코드 상향

--- android/MobiPay/app/build.gradle.kts
+++ android/MobiPay/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 28
         maxSdk = 34
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
