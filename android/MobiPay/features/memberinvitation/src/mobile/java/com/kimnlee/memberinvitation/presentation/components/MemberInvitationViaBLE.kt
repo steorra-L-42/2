a8fc7c5566d119f586ceb4c2954944c904ec8a23
Ak-mong <jeabin1129@naver.com>
2024-09-29 16:55:55 +0900

    [AND] Refactor: MemberInvitationViaBLE page add timer function

--- android/MobiPay/features/memberinvitation/src/mobile/java/com/kimnlee/memberinvitation/presentation/components/MemberInvitationViaBLE.kt
+++ android/MobiPay/features/memberinvitation/src/mobile/java/com/kimnlee/memberinvitation/presentation/components/MemberInvitationViaBLE.kt
@@ -6,13 +6,10 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -33,7 +30,14 @@ fun MemberInvitationViaBLE(
     viewModel: MemberInvitationViewModel,
     onNavigateToConfirmation: () -> Unit
 ) {
-    val showInvitationBLE by viewModel.showInvitationBLE.collectAsState()
+    var timeLeft by remember { mutableStateOf(5) } // 5초
+    LaunchedEffect(Unit) {
+        for (i in timeLeft downTo -1) {
+            delay(1000) // 1초 대기
+            timeLeft = i // 남은 시간 업데이트
+        }
+        viewModel.closeInvitationBLE()
+    }
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -42,18 +46,15 @@ fun MemberInvitationViaBLE(
         Text(
             text = "근처 멤버 초대",
             style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.weight(0.1f)
         )
         RadarAnimation(
             Modifier
                 .fillMaxSize() // 화면을 꽉 채움
-                .weight(0.8f)
-//                .background(Color.Black), // 배경색 설정
-
+                .weight(0.6f)
         )
+        Text(text = "남은 시간: $timeLeft 초", modifier = Modifier.weight(0.22f))
         // 테스트 코드 추가(구현 시 삭제 예정)
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(modifier = Modifier.weight(0.1f)) {
+        Row() {
             Button(
                 onClick = {
                     onNavigateToConfirmation()
@@ -78,39 +79,29 @@ fun MemberInvitationViaBLE(
 
 @Composable
 fun RadarAnimation(modifier: Modifier) {
-    // 무한 애니메이션 트랜지션 시작
-    val animationDuration = 60000L // 60,000 밀리초 = 60초
-    var isAnimating by remember { mutableStateOf(true) }
     val infiniteTransition = rememberInfiniteTransition()
-    // 애니메이션으로 아이콘의 크기 변화 설정
 
-    LaunchedEffect(Unit) {
-        delay(animationDuration)
-        isAnimating = false
-    }
+    // 애니메이션으로 아이콘의 크기 변화 설정
     val size by infiniteTransition.animateFloat(
-        initialValue = 0.99f, // 최소 크기
+        initialValue = 0.98f, // 최소 크기
         targetValue = 1.0f,  // 최대 크기
         animationSpec = infiniteRepeatable(
             animation = tween(300, easing = LinearEasing), // 1초간 애니메이션
             repeatMode = RepeatMode.Reverse // 크기 확장 후 다시 축소
         )
     )
-
-    if (isAnimating) {
-        Box(
-            modifier = modifier,
-            contentAlignment = Alignment.Center // 중앙 정렬
-        ) {
-            Icon(
-                painter = painterResource(R.drawable.trailer_solid), // 레이더 아이콘
-                contentDescription = null,
-                modifier = Modifier
-                    .size(300.dp) // 기본 크기 설정
-//                    .background(Color.Gray)
-                    .scale(size), // 애니메이션으로 크기 조절 (모든 방향으로 확장)
-                tint = Color.Blue // 아이콘 색상
-            )
-        }
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center // 중앙 정렬
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.baseline_radar_24), // 레이더 아이콘
+            contentDescription = null,
+            modifier = Modifier
+                .size(300.dp) // 기본 크기 설정
+                .scale(size), // 애니메이션으로 크기 조절 (모든 방향으로 확장)
+            tint = Color.Blue
+        )
     }
 }
+
