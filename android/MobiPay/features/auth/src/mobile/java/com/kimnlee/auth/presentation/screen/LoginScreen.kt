5eecc321aecc3edf16716f7ded9836f8f71ef076
Harvey <ssafy.lsc@gmail.com>
2024-09-29 23:48:56 +0900

    [AND] refactor: 로그인 화면 수정

--- android/MobiPay/features/auth/src/mobile/java/com/kimnlee/auth/presentation/screen/LoginScreen.kt
+++ android/MobiPay/features/auth/src/mobile/java/com/kimnlee/auth/presentation/screen/LoginScreen.kt
@@ -2,11 +2,14 @@ package com.kimnlee.auth.presentation.screen
 
 import android.app.Activity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,6 +19,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -23,16 +27,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kimnlee.auth.R
 import com.kimnlee.auth.presentation.viewmodel.LoginViewModel
+import com.kimnlee.common.ui.theme.MobiPayTheme
+import com.kimnlee.common.ui.theme.MobiTextAlmostBlack
+import com.kimnlee.common.ui.theme.MobiTextDarkGray
 
 @Composable
 fun LoginScreen(
@@ -51,80 +61,151 @@ fun LoginScreen(
         }
     }
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color.White
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 24.dp)
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.SpaceBetween
+    MobiPayTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color.White
         ) {
-            Column {
-                Spacer(modifier = Modifier.height(48.dp))
-                Text(
-                    text = "MobiPay",
-                    fontSize = 36.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = registrationButtonColor
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "간편 결제의 혁신",
-                    fontSize = 18.sp,
-                    color = Color.Gray
-                )
-            }
-
-            // 자동차 이미지
-            Image(
-                painter = painterResource(id = R.drawable.genesis_g90),
-                contentDescription = "Car Image",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(16f / 9f)
-                    .padding(vertical = 24.dp),
-                contentScale = ContentScale.Fit
-            )
-
             Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(30.dp)
+                    .verticalScroll(rememberScrollState()),
+//                verticalArrangement = Arrangement.SpaceBetween
             ) {
-                // 카카오 로그인 버튼 (기존 이미지 사용, 비율 조정)
+                Column {
+                    Spacer(modifier = Modifier.height(16.dp))
+//                    Text(
+//                        text = "모비페이",
+//                        style = MaterialTheme.typography.headlineMedium,
+////                        fontSize = 36.sp,
+//                        fontFamily = FontFamily(Font(com.kimnlee.common.R.font.psemibold)),
+//                        color = MobiTextDarkGray
+//                    )
+//                    Spacer(modifier = Modifier.height(2.dp))
+//                    Text(
+//                        text = "언제, 어디서나 결제는 간편하게",
+//                        fontSize = 16.sp,
+//                        color = Color.Gray
+//                    )
+//                    Spacer(modifier = Modifier.height(48.dp))
+//                    Row(
+//                        verticalAlignment = Alignment.CenterVertically,
+//                        modifier = Modifier
+//                            .padding(top = 2.dp)
+//                    ) {
+//                        Text(
+//                            text = " ⭐ ",
+//                            style = MaterialTheme.typography.headlineLarge,
+//                            fontFamily = FontFamily(Font(com.kimnlee.common.R.font.emoji)),
+////                            fontSize = 24.sp,
+//                            modifier = Modifier
+////                                .padding(top = 3.dp)
+//                        )
+//                        Text(
+//                            text = " 만나서 반가워요",
+//                            style = MaterialTheme.typography.headlineLarge,
+//                            color = MobiTextAlmostBlack,
+////                            fontSize = 24.sp,
+//                        )
+//                    }
+
+                    Text(
+                        text = "창문,",
+                        style = MaterialTheme.typography.headlineLarge,
+                        fontSize = 40.sp,
+                        fontFamily = FontFamily(Font(com.kimnlee.common.R.font.psemibold)),
+                        color = MobiTextDarkGray
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "내리지 마세요!",
+                        style = MaterialTheme.typography.headlineLarge,
+                        fontSize = 40.sp,
+                        fontFamily = FontFamily(Font(com.kimnlee.common.R.font.psemibold)),
+                        color = MobiTextDarkGray
+                    )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .padding(top = 2.dp)
+                    ) {
+                        Text(
+                            text = "모비페이",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = registrationButtonColor,
+                            fontSize = 18.sp,
+                        )
+                        Text(
+                            text = " 회원이라면 누구나 간편하게",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MobiTextAlmostBlack,
+                            fontSize = 18.sp,
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "번호판 인식만으로 결제할 수 있어요.",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MobiTextAlmostBlack,
+                        fontSize = 18.sp,
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(100.dp))
+
                 Image(
-                    painter = painterResource(id = R.drawable.kakao_login_large_wide),
-                    contentDescription = "카카오 로그인",
-                    contentScale = ContentScale.FillBounds,
+                    painter = painterResource(id = R.drawable.no_car_window),
+                    contentDescription = "창문 내리지 마세요 이미지",
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(48.dp)
-                        .clickable { viewModel.login(context as Activity) }
+                        .padding(horizontal = 50.dp),
+                    contentScale = ContentScale.FillWidth
                 )
 
-                // 회원가입 버튼
-                Button(
-                    onClick = { onNavigateToSignUp() },
+                Column(
+                    verticalArrangement = Arrangement.Bottom,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .height(48.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = registrationButtonColor),
-                    shape = RoundedCornerShape(8.dp)
+                        .fillMaxHeight()
+                        .weight(1f)
+                        .padding(horizontal = 8.dp)
+                        .align(Alignment.CenterHorizontally)
                 ) {
-                    Text("회원가입", fontSize = 16.sp, color = Color.White)
+                    Image(
+                        painter = painterResource(id = R.drawable.kakao_login_large_wide),
+                        contentDescription = "카카오 로그인",
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier
+                            .fillMaxWidth()
+//                            .height(48.dp)
+                            .clickable { viewModel.login(context as Activity) }
+                    )
+
+                    // 회원가입 버튼
+//                    Button(
+//                        onClick = { onNavigateToSignUp() },
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .height(48.dp),
+//                        colors = ButtonDefaults.buttonColors(containerColor = registrationButtonColor),
+//                        shape = RoundedCornerShape(8.dp)
+//                    ) {
+//                        Text("회원가입", fontSize = 16.sp, color = Color.White)
+//                    }
+//
+//                    // 테스트 로그인 버튼
+//                    TextButton(
+//                        onClick = { viewModel.testLogin() },
+//                        modifier = Modifier.fillMaxWidth()
+//                    ) {
+//                        Text("테스트 로그인", fontSize = 14.sp, color = registrationButtonColor)
+//                    }
                 }
 
-                // 테스트 로그인 버튼
-                TextButton(
-                    onClick = { viewModel.testLogin() },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("테스트 로그인", fontSize = 14.sp, color = registrationButtonColor)
-                }
+                Spacer(modifier = Modifier.height(10.dp))
             }
-
-            Spacer(modifier = Modifier.height(48.dp))
         }
     }
+
 }
