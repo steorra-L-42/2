9cbc1b930aa3b4677005473e3a82e05dc45ebba5
Harvey <ssafy.lsc@gmail.com>
2024-09-30 02:33:30 +0900

    [AND] refactor: 카카오 로그인 헤더 정보 가져오도록 수정

--- android/MobiPay/common/src/main/java/com/kimnlee/common/auth/repository/ApiUnAuthService.kt
+++ android/MobiPay/common/src/main/java/com/kimnlee/common/auth/repository/ApiUnAuthService.kt
@@ -3,10 +3,10 @@ package com.kimnlee.common.auth.repository
 import com.kimnlee.common.auth.AuthManager
 import com.kimnlee.common.auth.api.UnAuthService
 import com.kimnlee.common.auth.model.LoginRequest
-import com.kimnlee.common.auth.model.LoginResponse
 import com.kimnlee.common.auth.model.RegistrationRequest
 import com.kimnlee.common.auth.model.RegistrationResponse
 import com.kimnlee.common.network.ApiClient
+import retrofit2.Response
 
 class ApiUnAuthService(authManager: AuthManager) : UnAuthService {
 
@@ -14,7 +14,7 @@ class ApiUnAuthService(authManager: AuthManager) : UnAuthService {
         .unAuthenticatedApi
         .create(UnAuthService::class.java)
 
-    override suspend fun login(loginRequest: LoginRequest): LoginResponse {
+    override suspend fun login(loginRequest: LoginRequest): Response<Void> {
         return unAuthenticatedApi.login(loginRequest)
     }
 
