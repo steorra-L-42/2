6554ecccaf64f66cce6f90b9d1b62236a10f0805
김범중 <rlaqjawnd625@naver.com>
2024-09-30 15:30:17 +0900

    [BE] Feat: MobiPay서버 결제 알림 기능 추가

--- backend/mobipay_backend/src/main/java/com/example/mobipay/domain/cargroup/repository/CarGroupRepository.java
+++ backend/mobipay_backend/src/main/java/com/example/mobipay/domain/cargroup/repository/CarGroupRepository.java
@@ -1,10 +1,14 @@
 package com.example.mobipay.domain.cargroup.repository;
 
 import com.example.mobipay.domain.cargroup.entity.CarGroup;
+import com.example.mobipay.domain.cargroup.entity.CarGroupId;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarGroupRepository extends JpaRepository<CarGroup, Long> {
+public interface CarGroupRepository extends JpaRepository<CarGroup, CarGroupId> {
     Boolean existsByMobiUserIdAndCarId(Long mobiUserId, Long carId);
+
+    List<CarGroup> findByCarId(Long carId);
 }
