313c484ad16bf12ae1bfc4178c26e99ebd96ae23
김범중 <rlaqjawnd625@naver.com>
2024-10-01 00:00:07 +0900

    [BE] Feat: PaymentRequest 필드에 유효성 검사 추가 (최소값, 최대값, 크기)

--- backend/mobipay_backend/src/main/java/com/example/mobipay/domain/postpayments/dto/PaymentRequest.java
+++ backend/mobipay_backend/src/main/java/com/example/mobipay/domain/postpayments/dto/PaymentRequest.java
@@ -1,6 +1,8 @@
 package com.example.mobipay.domain.postpayments.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,9 +17,11 @@ public class PaymentRequest {
     private String type;
 
     @NotNull(message = "empty amount")
+    @Min(value = 1, message = "Payment balance must be greater than 0")
     private Long paymentBalance;
 
     @NotNull(message = "empty carNumber")
+    @Size(min = 7, max = 8, message = "Car number must be 7~8 characters")
     private String carNumber;
 
     @NotNull(message = "empty info")
