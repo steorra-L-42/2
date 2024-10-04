af464cb8bafbc291e90692ede217c77db2dc122e
김의근 <fkaus4598@naver.com>
2024-10-05 06:30:26 +0900

    [BE] Feat: Add MerchantTransaction and MerchantTranscactionResponse DTOs

---
+++ backend/merchant_backend/src/main/java/com/example/merchant/domain/cancel/dto/MerchantTranscactionResponse.java
@@ -0,0 +1,16 @@
+package com.example.merchant.domain.cancel.dto;
+
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Getter
+public class MerchantTranscactionResponse {
+
+    private List<MerchantTransaction> items;
+
+}
