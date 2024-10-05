69762d3569214809b1b4d8037c5d111e8123aa2d
Harvey <ssafy.lsc@gmail.com>
2024-10-05 20:08:41 +0900

    [AND] commit to merge

--- android/MobiPay/common/src/main/java/com/kimnlee/common/auth/repository/ApiAuthService.kt
+++ android/MobiPay/common/src/main/java/com/kimnlee/common/auth/repository/ApiAuthService.kt
@@ -3,6 +3,8 @@ package com.kimnlee.common.auth.repository
 import com.kimnlee.common.auth.AuthManager
 import com.kimnlee.common.auth.api.AuthService
 import com.kimnlee.common.auth.model.LoginRequest
+import com.kimnlee.common.auth.model.LoginResponse
+import com.kimnlee.common.auth.model.RegisterResponse
 import com.kimnlee.common.auth.model.RegistrationRequest
 import com.kimnlee.common.auth.model.SendTokenRequest
 import com.kimnlee.common.auth.model.SendTokenResponse
@@ -14,11 +16,11 @@ class ApiAuthService(
     private val authenticatedApi: AuthService
 ) : AuthService {
 
-    override suspend fun login(loginRequest: LoginRequest): Response<Void> {
+    override suspend fun login(loginRequest: LoginRequest): Response<LoginResponse> {
         return unAuthenticatedApi.login(loginRequest)
     }
 
-    override suspend fun register(registrationRequest: RegistrationRequest): Response<Void> {
+    override suspend fun register(registrationRequest: RegistrationRequest): Response<RegisterResponse> {
         return unAuthenticatedApi.register(registrationRequest)
     }
 
