f07698d5cf55147f5256062cc06cbecd6953b01f
김범중 <rlaqjawnd625@naver.com>
2024-10-07 17:13:09 +0900

    [BE] Feat: 소유한 카드 조회 동의 상태 조회 기능 개발

--- backend/mobipay_backend/src/main/java/com/example/mobipay/domain/mobiuser/controller/MobiUserController.java
+++ backend/mobipay_backend/src/main/java/com/example/mobipay/domain/mobiuser/controller/MobiUserController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,15 @@ public class MobiUserController {
             @AuthenticationPrincipal CustomOAuth2User oauth2User) {
 
         MyDataConsentResponse response = mobiUserService.approveMyDataConsent(oauth2User);
+        return ResponseEntity.ok(response);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/mydata-consent")
+    public ResponseEntity<MyDataConsentResponse> getMyDataConsent(
+            @AuthenticationPrincipal CustomOAuth2User oauth2User) {
+
+        MyDataConsentResponse response = mobiUserService.getMyDataConsent(oauth2User);
         return ResponseEntity.ok(response);
     }
 }
