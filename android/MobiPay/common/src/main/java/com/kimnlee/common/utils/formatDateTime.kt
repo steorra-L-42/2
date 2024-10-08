ee8b0230b09d5f77999ad2db267a1d68fbcae135
김세진 <rlatpwls30@naver.com>
2024-10-08 23:07:14 +0900

    [AND] Feat: 전자 영수증에서 사용할 dateFormat 추가

--- android/MobiPay/common/src/main/java/com/kimnlee/common/utils/formatDateTime.kt
+++ android/MobiPay/common/src/main/java/com/kimnlee/common/utils/formatDateTime.kt
@@ -8,3 +8,9 @@ fun formatDateTime(date: String, time: String): String {
     val parsedDateTime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
     return parsedDateTime.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH:mm:ss"))
 }
+
+fun formatDateTimeWithHyphens(date: String, time: String): String {
+    val dateTimeString = "$date$time"
+    val parsedDateTime = LocalDateTime.parse(dateTimeString, DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
+    return parsedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+}
