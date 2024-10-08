697473c21bfc0ad2eaae0245f2eb604a26624ac3
Harvey <ssafy.lsc@gmail.com>
2024-10-09 02:22:30 +0900

    [AND]feat: 수동결제 Alert 처리 버그 수정

--- android/MobiPay/features/payment/src/main/java/com/kimnlee/payment/PaymentApprovalReceiver.kt
+++ android/MobiPay/features/payment/src/main/java/com/kimnlee/payment/PaymentApprovalReceiver.kt
@@ -10,6 +10,7 @@ import com.kimnlee.common.FCMDependencyProvider
 private const val TAG = "PaymentApprovalReceiver"
 class PaymentApprovalReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d(TAG, "onReceive: Broadcast Receive 했다.")
         if (intent?.action == "com.kimnlee.mobipay.PAYMENT_APPROVAL" && context != null) {
             Log.d("PaymentApprovalReceiver", "사용자가 결제 승인함")
 
