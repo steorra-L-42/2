d8dff04a60410c3880392b0b95a3e2d8e4959f86
이진송 <annaring30@naver.com>
2024-10-08 16:39:52 +0900

    Feat: SecurityConfig 경로별 인가 미적용

--- backend/mobipay_backend/src/main/java/com/example/mobipay/config/SecurityConfig.java
+++ backend/mobipay_backend/src/main/java/com/example/mobipay/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -118,10 +117,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers(HttpMethod.POST, whitelist_post).permitAll()
-                        .requestMatchers(HttpMethod.GET, whitelist_get).permitAll()
-                        .requestMatchers(HttpMethod.PATCH, whitelist_patch).permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().permitAll())
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(customAuthenticationEntryPoint) // 커스텀 AuthenticationEntryPoint 설정
                 );
